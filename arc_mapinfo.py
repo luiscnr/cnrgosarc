50a7818f0e63b8390258cda067f8cc7528ed7832
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2023-01-30 16:46:51 +0100

    Improved copy systems

--- arc_mapinfo.py
+++ arc_mapinfo.py
@@ -276,41 +276,48 @@ class ArcMapInfo:
             return dst
         if self.verbose:
             print(f'[INFO] Copying file grid: {self.ifile_base}...')
-        with Dataset(self.ifile_base) as src:
-            dst = Dataset(ofile, 'w', format='NETCDF4')
-            # copy global attributes all at once via dictionary
-            dst.setncatts(src.__dict__)
 
-            # copy dimensions
-            for name, dimension in src.dimensions.items():
-                dst.createDimension(
-                    name, (len(dimension) if not dimension.isunlimited() else None))
-
-            # copy all file data except for the excluded
-            for name, variable in src.variables.items():
-                dst.createVariable(name, variable.datatype, variable.dimensions, fill_value=-999, zlib=True,
-                                   shuffle=True, complevel=4)
-                # copy variable attributes all at once via dictionary
-                dst[name].setncatts(src[name].__dict__)
-
-                dst[name][:] = src[name][:]
+        # with Dataset(self.ifile_base) as src:
+        #     dst = Dataset(ofile, 'w', format='NETCDF4')
+        #     # copy global attributes all at once via dictionary
+        #     dst.setncatts(src.__dict__)
+        #
+        #     # copy dimensions
+        #     for name, dimension in src.dimensions.items():
+        #         if self.verbose:
+        #             print(f'[INFO] -> Copying dimension: {name}')
+        #         dst.createDimension(
+        #             name, (len(dimension) if not dimension.isunlimited() else None))
+        #
+        #     # copy all file data except for the excluded
+        #     for name, variable in src.variables.items():
+        #         if self.verbose:
+        #             print(f'[INFO] -> Copying variable: {name}')
+        #         dst.createVariable(name, variable.datatype, variable.dimensions, fill_value=-999, zlib=True,
+        #                            shuffle=True, complevel=4)
+        #         # copy variable attributes all at once via dictionary
+        #         dst[name].setncatts(src[name].__dict__)
+        #
+        #         dst[name][:] = src[name][:]
 
         # shutil.copy(self.ifile_base,ofile)
 
-        # cmd = f'cp -a {self.ifile_base} {ofile}'
-        # import subprocess
-        # import time
-        # prog = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
-        # #prog.wait()
-        # originalSize = os.path.getsize(self.ifile_base)
-        # historicalSize = -1
-        # while historicalSize != originalSize:
-        #     if os.path.exists(ofile):
-        #         historicalSize = os.path.getsize(ofile)
-        #         print('aqui...', historicalSize, originalSize)
-        #     time.sleep(1)
-        # print('copied')
-        # dst = Dataset(ofile, 'a', format='NETCDF4')
+        cmd = f'cp -a {self.ifile_base} {ofile}'
+        import subprocess
+        import time
+        subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+        originalSize = os.path.getsize(self.ifile_base)
+        historicalSize = -1
+        while historicalSize != originalSize:
+            if os.path.exists(ofile):
+                historicalSize = os.path.getsize(ofile)
+                if self.verbose:
+                    porc = (historicalSize/originalSize)*100
+                    print(f'Copying {porc:.2f} %')
+            time.sleep(1)
+        if self.verbose:
+            print('[INFO] Copy completed')
+        dst = Dataset(ofile, 'a', format='NETCDF4')
         return dst
 
     def save_quick_look_impl(self, fileout, data):
