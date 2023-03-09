b377c719ccf7529da0b42a747d2f3738c18e5ee8
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2023-03-09 16:46:54 +0100

    Added monthly computation option

--- arc_processing.py
+++ arc_processing.py
@@ -109,7 +109,7 @@ class ArcProcessing:
                 count_array = np.array(var_avg_count[0, limits[0]:limits[1], limits[2]:limits[3]])
                 error_array = np.array(var_avg_error[0, limits[0]:limits[1], limits[2]:limits[3]])
                 print(array.shape)
-                mask_array =np.array(var_smask[limits[0]:limits[1], limits[2]:limits[3]])
+                mask_array =np.array(var_smask[0,limits[0]:limits[1], limits[2]:limits[3]])
                 print(var_smask.shape)
                 print(mask_array.shape)
 
